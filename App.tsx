abd62be23eed75c37d52254ef4a2b37246d026c9
pcaldi <pcaldi@gmail.com>
2025-02-14 22:21:02 -0300

    finalizando o componente

--- App.tsx
+++ App.tsx
@@ -1,10 +1,44 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { useRef } from 'react';
+import { StyleSheet, View, TextInput } from 'react-native';
+import Rive, { Fit, RiveRef } from 'rive-react-native';
 
 export default function App() {
+
+  const riveRef = useRef<RiveRef>(null)
+
+  function changeAnimation(isTyping: boolean) {
+
+    riveRef.current?.setInputState("State Machine 1", "Boolean 1", isTyping);
+
+  }
+
+
   return (
     <View style={styles.container}>
-      <Text>Rive App</Text>
+      <Rive
+        resourceName='password_input'
+        style={{
+          width: '100%',
+          maxHeight: 330,
+        }}
+        autoplay={true}
+        fit={Fit.Cover}
+        ref={riveRef}
+      />
+      <TextInput
+        placeholder='Seu@email.com'
+        style={styles.input}
+        autoCapitalize='none'
+        autoCorrect={false}
+        textContentType='emailAddress'
+      />
+      <TextInput
+        placeholder='Sua senha'
+        style={styles.input}
+        secureTextEntry
+        onFocus={() => changeAnimation(true)}
+        onBlur={() => changeAnimation(false)}
+      />
     </View>
   );
 }
@@ -15,5 +49,16 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    padding: 12,
+    gap: 12,
   },
+  input: {
+    width: '100%',
+    height: 56,
+    borderRadius: 8,
+    borderWidth: 1,
+    borderColor: '#C4C4C4',
+    paddingHorizontal: 16,
+    fontSize: 16,
+  }
 });
